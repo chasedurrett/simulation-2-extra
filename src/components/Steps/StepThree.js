3805f62d619fbb8bdf74281baa3cb388979514b0
chasedurrett <chase.durrett80@gmail.com>
2020-06-12 22:38:27 -0500

    second commit

--- src/components/Steps/StepThree.js
+++ src/components/Steps/StepThree.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import {Link} from 'react-router-dom'
-import axios from 'axios'
+import axios from "axios";
 
 class StepThree extends Component {
   constructor() {
